b7fe2280a154b258815f1d18d81df8a58d3f011d
Nikolas Martens <Nikolas.M@rtens.org>
2026-03-04 08:23:04 +0100

    Create output in tests

--- test.js
+++ test.js
@@ -5,30 +5,30 @@ import generate_reverse_dictionary from './src/generate_reverse_dictionary.js'
 import generate_forward_dictionary from './src/generate_forward_dictionary.js'
 
 test('bases', t => {
-		generate_bases(null)
-		t.pass()
+	generate_bases('out/bases.html')
+	t.pass()
 })
 
 test('forward dictionary', t => {
-		const logged = []
-		generate_forward_dictionary(null, {
-				log: (...args) => logged.push(args)
-		})
-		t.deepEqual(logged, [])
+	const logged = []
+	generate_forward_dictionary('out/dictionary.html', {
+		log: (...args) => logged.push(args)
+	})
+	t.deepEqual(logged, [])
 })
 
 test('reverse dictionary', t => {
-		const logged = []
-		generate_reverse_dictionary(null, {
-				log: (...args) => logged.push(args)
-		})
-		t.deepEqual(logged, [])
+	const logged = []
+	generate_reverse_dictionary('out/dictionary_reverse.html', {
+		log: (...args) => logged.push(args)
+	})
+	t.deepEqual(logged, [])
 })
 
 test('sentences', t => {
-		const logged = []
-		generate_sentences(null, {
-				log: (...args) => logged.push(args)
-		})
-		t.deepEqual(logged, [])
+	const logged = []
+	generate_sentences('out/sentences.html', {
+		log: (...args) => logged.push(args)
+	})
+	t.deepEqual(logged, [])
 })
